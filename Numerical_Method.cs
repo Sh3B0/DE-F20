634e323e217bd1371db711ab3d52ffb0ccd7346b
Sh3b0 <ahmedshaban784@gmail.com>
2020-10-20 03:12:54 +0300

    Reorganize project files

--- Numerical_Method.cs
+++ Numerical_Method.cs
@@ -3,6 +3,7 @@ using System.Windows.Forms;
 
 namespace Differential_Equations
 {
+    // The main class that contains common fields and methods needed by all descendents
     public class Numerical_Method
     {
         public double[] x, y;
@@ -70,108 +71,6 @@ namespace Differential_Equations
                 MessageBox.Show("Overflow: Calculations exceeded the range for Double data type");
                 Application.Exit();
             }
-        }
-    }
-
-    public class Euler : Numerical_Method
-    {
-        public new double[] x, y;
-        public double[] e;
-        public Euler(int N, double x0, double y0, double X) : base(N, x0, y0, X)
-        {
-            x = new double[N + 1];
-            y = new double[N + 1];
-            e = new double[N + 1];
-            x[0] = x0;
-            y[0] = y0;
-        }
-
-        // Prepares the XY values for Euler method graphs.
-        public new double Calculate()
-        {
-            double mxe = 0;
-            for (int i = 1; i < N; i++)
-            {
-                x[i] = x[i - 1] + h;
-                y[i] = y[i - 1] + h * F(x[i - 1], y[i - 1]);
-
-                e[i] = Math.Abs(y[i] - Exact(x[i]));
-                mxe = Math.Max(mxe, e[i]);
-
-                CheckOverflow(x[i], y[i], e[i]);
-
-                // Debug.WriteLine("EM: " + x[i] + " " + y[i]);
-            }
-            return mxe;
-        }
-    }
-    public class Improved_Euler : Numerical_Method
-    {
-        public new double[] x, y;
-        public double[] e;
-        public Improved_Euler(int N, double x0, double y0, double X) : base(N, x0, y0, X)
-        {
-            x = new double[N + 1];
-            y = new double[N + 1];
-            e = new double[N + 1];
-            x[0] = x0;
-            y[0] = y0;
-        }
-
-        // Prepares the XY values for Improved Euler method graphs.
-        public new double Calculate()
-        {
-            double mxe = 0;
-            for (int i = 1; i < N; i++)
-            {
-                x[i] = x[i - 1] + h;
-                y[i] = y[i - 1] + h / 2.0 * (F(x[i - 1], y[i - 1]) + F(x[i - 1] + h, y[i - 1] + h * F(x[i - 1], y[i - 1])));
-                e[i] = Math.Abs(y[i] - Exact(x[i]));
-
-                mxe = Math.Max(mxe, e[i]);
-
-                CheckOverflow(x[i], y[i], e[i]);
-
-                //Debug.WriteLine("IEM: " + x[i] + " " + y[i]);
-            }
-            return mxe;
-        }
-    }
-    public class Runge_Kutta : Numerical_Method
-    {
-        public new double[] x, y;
-        public double[] e;
-        public Runge_Kutta(int N, double x0, double y0, double X) : base(N, x0, y0, X)
-        {
-            x = new double[N + 1];
-            y = new double[N + 1];
-            e = new double[N + 1];
-            x[0] = x0;
-            y[0] = y0;
-        }
-
-        // Prepares the XY values for Runge-Kutta method graphs.
-        public new double Calculate()
-        {
-            double mxe = 0;
-            for (int i = 1; i < N; i++)
-            {
-                x[i] = x[i - 1] + h;
-                double k1, k2, k3, k4;
-                k1 = F(x[i - 1], y[i - 1]);
-                k2 = F(x[i - 1] + h / 2.0, y[i - 1] + h / 2.0 * k1);
-                k3 = F(x[i - 1] + h / 2.0, y[i - 1] + h / 2.0 * k2);
-                k4 = F(x[i - 1] + h, y[i - 1] + h * k3);
-                y[i] = y[i - 1] + h / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4);
-
-                e[i] = Math.Abs(y[i] - Exact(x[i]));
-                mxe = Math.Max(mxe, e[i]);
-
-                CheckOverflow(x[i], y[i], e[i]);
-
-                //Debug.WriteLine("RKM: " + x[i] + " " + y[i]);
-            }
-            return mxe;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Differential_Equations
@@ -16,7 +13,7 @@ namespace Differential_Equations
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
